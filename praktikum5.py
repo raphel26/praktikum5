b2077de3c51ce56c62b13c73fe6c253ce2e4bae0
raphel26 <115516830+raphel26@users.noreply.github.com>
2022-11-28 11:16:18 +0700

    Add files via upload

---
+++ praktikum5.py
@@ -0,0 +1,106 @@
+mahasiswa = {}
+
+def show():
+    if mahasiswa.items():
+        print("Daftar Nilai")
+        print("=================================================================================")
+        print("| No |      Nama      |     NIM     |  Tugas  |   UTS   |   UAS   |    Akhir    |")
+        print("=================================================================================")
+        i = 0
+        for a in mahasiswa.items():
+            i += 1
+            print("| {no:2d} | {0:14s} | {1:11s} | {2:7d} | {3:7d} | {4:7d} |      {5:6.2f} |"
+            .format (a[0][: 14],a[1][0],a[1][1],a[1][2],a[1][3],a[1][4], no = i))
+        print("=================================================================================")
+        
+    else:
+        print("Daftar Nilai")
+        print("=================================================================================")
+        print("| No |      Nama      |     NIM     |  Tugas  |   UTS   |   UAS   |    Akhir    |")
+        print("=================================================================================")
+        print("|                                TIDAK ADA DATA                                 |")
+        print("=================================================================================")
+
+def add():
+    print("Tambah Data")
+    nama = input("Nama\t : ")
+    nim = input("NIM\t : ")
+    uts = int(input("Nilai UTS\t : "))
+    uas = int(input("Nilai UAS\t : "))
+    tugas = int(input("Nilai Tugas\t : "))
+    akhir = (tugas * 30/100) + (uts * 35/100) + (uas * 35/100)
+    mahasiswa[nama] = nim, tugas, uts, uas, akhir
+
+def delete():
+    print("Hapus Data")
+    nama = input("Masukkan Nama : ")
+    
+    if nama in mahasiswa.keys():
+        del mahasiswa[nama]
+    
+    else:
+        print("Nama tidak ditemukan")
+
+def update():
+    print("Ubah Data")
+    nama = input("Masukkan Nama : ")
+    if nama in mahasiswa.keys():
+        nim = input("NIM\t : ")
+        uts = int(input("Nilai UTS\t : "))
+        uas = int(input("Nilai UAS\t : "))
+        tugas = int(input("Nilai Tugas\t : "))
+        akhir = (tugas * 30/100) + (uts * 35/100) + (uas * 35/100)
+        mahasiswa[nama] = nim, tugas, uts, uas, akhir
+
+    else:
+        print("Nama tidak ditemukan ")
+
+def search():
+    print("Cari Data")
+    a = input("Masukkan Nama : ")
+    if a in mahasiswa.keys():
+        print("===========================================================================")
+        print("|      Nama      |     NIM     |  Tugas  |   UTS   |   UAS   |    Akhir   |")
+        print("===========================================================================")
+        print("| {0:14s} | {1:11s} | {2:7d} | {3:7d} | {4:7d} |     {5:6.2f} |"
+            .format (a , mahasiswa[a][0], mahasiswa[a][1], mahasiswa[a][2], mahasiswa[a][3], mahasiswa[a][4] ))
+        print("===========================================================================")
+
+    else:
+        print("=================================================================================")
+        print("| No |      Nama      |     NIM     |  Tugas  |   UTS   |   UAS   |    Akhir    |")
+        print("=================================================================================")
+        print("|                          DATA TIDAK DITEMUKAN                                 |")
+        print("=================================================================================")
+
+def menu():
+    
+
+    x = input("[(L)ihat, (T)ambah, (U)bah, (H)apus, (C)ari, (K)eluar]: ")
+    print("\n")
+
+    if x == 'L':
+        show()
+    elif x == 'T':
+        add()
+    elif x == 'U':
+        update()
+    elif x == 'H':
+        delete()
+    elif x == 'C':
+        search()
+    elif x == 'K':
+        
+        print('\n')
+        print("> You exit the code                        ")
+        print("\n")
+        
+
+        exit()
+
+    else:
+        print("            Kode Yang Anda Masukan Tidak Valid                   ")
+        print("\n")
+
+while True:
+    menu()
